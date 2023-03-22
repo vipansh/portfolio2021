a2b38f5ae15cb8e765ceec8dfc339a979eb3d24b
Vipansh Thakur <66291636+vipansh@users.noreply.github.com>
2023-03-22 09:36:57 +0530

    ✅ Update:aboute me

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -1,27 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 export const AboutMe = () => {
-    return (
-
-
-
-        < section className="text-gray-300 body-font" >
-            <div className="containerMd px-5 py-24 mx-auto flex flex-col md:flex-row ">
-                <div className=" w-full lg:w-1/2 mb-10 lg:mb-0 rounded-lg h-1/2 overflow-hidden p-4">
-                    <img alt="feature" className="object-cover object-center rounded-lg  w-full" src="assets/mypic.jpeg" />
-                </div>
-                <div className=" w-full flex-wrap lg:w-auto mx-4 p-4 flex justify-center align-center m-auto">
-                    <div className="w-full text-xl font-medium text-center md:text-left text-text-primary sm:text-2xl md:text-3xl lg:text-4xl">About Me</div>
-                    <div >
-                        <p className="text-lg my-16 font-normal sm:text-xl lg:text-2xl xl:text-2-5xl text-text-secondary1">  I am a self-taught front-end developer looking for a role in an exciting company.</p>
-
-                        <p className="text-lg my-16 font-normal sm:text-xl lg:text-2xl xl:text-2-5xl text-text-secondary1">      When writing Javscript, I prefer React.       </p>
-
-                        <p className="text-lg my-16 font-normal sm:text-xl lg:text-2xl xl:text-2-5xl text-text-secondary1">      While I'm not coding, you'll find me reading fiction, reading Hindi literatures, or studying maths.           </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-    )
-}
+  return (
+    <section className="text-gray-300 body-font">
+      <div className="containerMd px-5 py-24 mx-auto flex flex-col md:flex-row">
+        <div className="w-full lg:w-1/2 mb-10 lg:mb-0 rounded-lg overflow-hidden p-4">
+          <img alt="feature" className="object-cover object-center rounded-lg w-full h-full" src="assets/mypic.png" />
+        </div>
+        <div className="w-full lg:w-1/2 mx-auto p-4 flex flex-col justify-center">
+          <div className="text-2xl md:text-3xl lg:text-4xl font-bold text-center md:text-left mb-8">
+            Meet Me
+          </div>
+          <div className="text-lg font-normal sm:text-xl lg:text-2xl xl:text-2-5xl text-text-secondary1 mb-8">
+            I'm a front-end developer, which means I'm a master of turning coffee into code. I can code circles around most people, but I also know how to have fun. 
+          </div>
+          <div className="text-lg font-normal sm:text-xl lg:text-2xl xl:text-2-5xl text-text-secondary1 mb-8">
+            When I'm not coding, you might find me at a coffee shop, sipping on something fancy and pretending to know what I'm talking about. Or maybe I'm playing chess, pretending to be a master strategist while secretly hoping my opponent makes a mistake. Either way, I'm having a good time!
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
